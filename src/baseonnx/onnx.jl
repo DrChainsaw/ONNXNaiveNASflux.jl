c1c3eb10fe69836f591aa9da8fd964502153f525
Andrew Lambe <andrew@abledatascience.ca>
2023-10-04 11:50:24 -0400

    WIP: only need one of the PB files, it seems

--- src/baseonnx/onnx.jl
+++ src/baseonnx/onnx.jl
@@ -1,11 +1,11 @@
 module onnx
 
-include("onnx-ml3_pb.jl")
+# include("onnx-ml3_pb.jl")
 include("onnx3_pb.jl")
-include("onnx-ml3_pb.jl")
-include("onnx3_pb.jl")
-include("onnx-operators-ml3_pb.jl")
-include("onnx-data3_pb.jl")
-include("onnx-operators3_pb.jl")
+# include("onnx-ml3_pb.jl")
+# include("onnx3_pb.jl")
+# include("onnx-operators-ml3_pb.jl")
+# include("onnx-data3_pb.jl")
+# include("onnx-operators3_pb.jl")
 
 end # module onnx
