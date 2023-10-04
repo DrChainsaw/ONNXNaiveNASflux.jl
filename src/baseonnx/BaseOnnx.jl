b60f5734a039902086bb04da05eedc69c2bce763
Andrew Lambe <andrew@abledatascience.ca>
2023-10-04 12:01:48 -0400

    WIP: precompilation works with major warning - function in write.jl is overwriting an onnx3_pb.jl function

--- src/baseonnx/BaseOnnx.jl
+++ src/baseonnx/BaseOnnx.jl
@@ -1,7 +1,7 @@
 module BaseOnnx
   const _ProtoBuf_Top_ = @static isdefined(parentmodule(@__MODULE__), :_ProtoBuf_Top_) ? (parentmodule(@__MODULE__))._ProtoBuf_Top_ : parentmodule(@__MODULE__)
   # include("onnx_pb.jl")
-  include("onnx.jl")
+  include("onnx3_pb.jl")
   include("read.jl")
   include("write.jl")
 end
--- src/baseonnx/read.jl
+++ src/baseonnx/read.jl
@@ -8,39 +8,39 @@ function array(p::TensorProto, wrap=Array)
     # Copy pasted from jl
     # Can probably be cleaned up a bit 
     # TODO: Add missing datatypes...
-    if p.data_type === TensorProto_DataType.INT64
+    if p.data_type === var"TensorProto.DataType".INT64
         if hasproperty(p, :int64_data) && !isempty(p.int64_data)
             return reshape(reinterpret(Int64, p.int64_data), reverse(p.dims)...) |> wrap
         end
         return reshape(reinterpret(Int64, p.raw_data), reverse(p.dims)...) |> wrap
     end
 
-    if p.data_type === TensorProto_DataType.INT32
+    if p.data_type === var"TensorProto.DataType".INT32
         if hasproperty(p, :int32_data) && !isempty(p.int32_data)
             return reshape(p.int32_data , reverse(p.dims)...) |> wrap
         end
         return reshape(reinterpret(Int32, p.raw_data), reverse(p.dims)...) |> wrap
     end
 
-    if p.data_type === TensorProto_DataType.INT8
+    if p.data_type === var"TensorProto.DataType".INT8
         return reshape(reinterpret(Int8, p.raw_data), reverse(p.dims)...) |> wrap
     end
 
-    if p.data_type === TensorProto_DataType.DOUBLE
+    if p.data_type === var"TensorProto.DataType".DOUBLE
         if hasproperty(p, :double_data) && !isempty(p.double_data)
             return reshape(p.double_data , reverse(p.dims)...) |> wrap
         end
         return reshape(reinterpret(Float64, p.raw_data), reverse(p.dims)...) |> wrap
     end
 
-    if p.data_type === TensorProto_DataType.FLOAT
+    if p.data_type === var"TensorProto.DataType".FLOAT
         if hasproperty(p,:float_data) && !isempty(p.float_data)
             return reshape(reinterpret(Float32, p.float_data), reverse(p.dims)...) |> wrap
         end
         return reshape(reinterpret(Float32, p.raw_data), reverse(p.dims)...) |> wrap
     end
 
-    if p.data_type === TensorProto_DataType.FLOAT16
+    if p.data_type === var"TensorProto.DataType".FLOAT16
         return reshape(reinterpret(Float16, p.raw_data), reverse(p.dims)...) |> wrap
     end
 end
@@ -48,9 +48,9 @@ end
 Base.size(vip::ValueInfoProto) = size(vip._type)
 Base.size(tp::TypeProto) = size(tp.tensor_type)
 Base.size(tp::TensorProto) = tp.dims
-Base.size(tp_t::TypeProto_Tensor) = hasproperty(tp_t, :shape) ? size(tp_t.shape) : missing
+Base.size(tp_t::var"TypeProto.Tensor") = hasproperty(tp_t, :shape) ? size(tp_t.shape) : missing
 Base.size(tsp::TensorShapeProto) = size.(Tuple(reverse(tsp.dim)))
-Base.size(tsp_d::TensorShapeProto_Dimension) = hasproperty(tsp_d, :dim_value) ? tsp_d.dim_value : missing
+Base.size(tsp_d::var"TensorShapeProto.Dimension") = hasproperty(tsp_d, :dim_value) ? tsp_d.dim_value : missing
 
 """
     attribute(p::AttributeProto) 
